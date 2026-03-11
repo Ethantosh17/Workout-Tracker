bf5ce6a329969196d5107c5323ad8c9e06368c68
Ethan Bechner <ebech17@gmail.com>
2026-03-11 07:01:52 -0500

    restructured directory

---
+++ Workout TrackerTests/Workout_TrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Workout_TrackerTests.swift
+//  Workout TrackerTests
+//
+//  Created by big money ethan on 3/9/26.
+//
+
+import Testing
+
+struct Workout_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
